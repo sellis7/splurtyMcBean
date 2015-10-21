c15eeec7189b7a95a6c94338e1e23f92ed9e3ef2
Sherilyn <studedlux@yahoo.com>
2015-10-21 18:38:11 +0000

    form validation & more css tweaking

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,4 +6,19 @@ class QuotesController < ApplicationController
 	def new 	# new "action" for user to submit to database
 		@quote = Quote.new
 	end
+
+	def create
+		@quote = Quote.create(quote_params)
+		if @quote.invalid? 	# user error message
+			flash[:error] = '<strong>Quote will not be stored.</strong> Your entry is invalid.'
+		end
+		redirect_to root_path
+	end
+
+	private
+
+	def quote_params
+		params.require(:quote).permit(:saying, :author)
+	end
+
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+	validates :saying, :presence => true, :length => { :maximum => 350, :minimum => 5 }
+	validates :author, :presence => true, :length => { :maximum => 50, :minimum => 2 }
 end
