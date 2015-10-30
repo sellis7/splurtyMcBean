ef473918621b5afa00c40e384dacd2869f5c4c42
Sherilyn <studedlux@yahoo.com>
2015-10-30 19:04:37 +0000

    3 add'l Bootstrap components; 1 functional

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,10 +8,22 @@ class QuotesController < ApplicationController
 	# 	@quote = Quote.new
 	# end
 
+# Was working on how to trigger previous & next buttons #
+#	def previous
+#  Quote.where(["id < ?", id]).last
+# end
+
+# def next
+#  Quote.where(["id > ?", id]).first
+# end
+
 	def create
 		@quote = Quote.create(quote_params)
+
 		if @quote.invalid? 	# user error message
 			flash[:error] = '<strong>The quote will not be stored.</strong> Your entry is invalid.'
+		else
+			flash[:notice] = '<span class="glyphicon glyphicon-ok"></span><strong> Thank you!</strong> Your entry has been saved.'
 		end
 		redirect_to root_path
 	end
