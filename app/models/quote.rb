65f641f74f6e888888390bb7e3b50d69595f4191
sellis7 <sellis7@users.noreply.github.com>
2015-11-20 17:12:40 -0500

    Completion of a basic 'asset_equal' Model test

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,13 @@
 class Quote < ActiveRecord::Base
 	validates :saying, :presence => true, :length => { :maximum => 350, :minimum => 5 }
 	validates :author, :presence => true, :length => { :maximum => 50, :minimum => 2 }
+
+	def unique_tag
+		abbr = self.author.split(" ").collect do |sub_string|
+			sub_string[0]
+		end
+		abbr.join + '#' + self.id.to_s
+
+	end
+
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+
+  test "unique_tag" do
+    quote = Quote.create(:author =>'Boomtown Rats', :saying => 'I don\'t like Mondays.')
+    expected = 'BR#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
+
+    # puts quote.inspect
   end
+
+
+
 end
